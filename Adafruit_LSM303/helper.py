8e4d6c6a8e29879aa318b840d47fa6d095d0a349
eloylp <eloy@sandboxwebs.com>
2017-08-24 11:35:51 +0200

    adding compass helper class and tests.

---
+++ Adafruit_LSM303/helper.py
@@ -0,0 +1,17 @@
+from math import atan2, pi
+
+
+class Compass:
+    def __init__(self, lsm303_sensor):
+        """
+        :type lsm303_sensor : Adafruit_LSM303.LSM303.LSM303
+        """
+        self.lsm303_sensor = lsm303_sensor
+
+    def get_heading(self):
+        magnet_axis_data = self.lsm303_sensor.read_magnetometer()
+        heading_radians = atan2(magnet_axis_data[1], magnet_axis_data[0])
+        heading_degrees = heading_radians * 180 / pi
+        if heading_degrees < 0:
+            heading_degrees = 360 + heading_degrees
+        return heading_degrees
---
+++ test/test_compass.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+from ddt import ddt, data, unpack
+from mock import MagicMock
+
+from Adafruit_LSM303.helper import Compass
+
+
+@ddt
+class TestCompass(TestCase):
+    @data(
+        (0, 0, 0),
+        (1, 0, 0),
+        (1, 1, 45),
+        (0, 1, 90),
+        (-1, 0, 180),
+        (-1, -1, 225),
+        (0, -1, 270),
+        (1, -0.0000000000000001, 360),
+    )
+    @unpack
+    def test_get_heading(self, x, y, expected_degrees):
+
+        lsm303 = MagicMock()
+        lsm303.read_magnetometer = MagicMock(return_value=(x, y))
+        compass = Compass(lsm303)
+        heading = compass.get_heading()
+        self.assertEqual(expected_degrees, heading)
