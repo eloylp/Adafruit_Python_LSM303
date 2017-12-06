7de6b1132ee84570b537da66e16427de54aad7d8
eloylp <eloy@sandboxwebs.com>
2017-12-06 01:00:01 +0100

    removing measure adapter.

--- test/test_instrument.py
+++ test/test_instrument.py
@@ -26,26 +26,3 @@ class TestInstrument(TestCase):
     def test_vector_2_degrees(self, x, y, expected_degrees):
         degrees = self.instrument.vector_2_degrees(x, y)
         self.assertEqual(expected_degrees, degrees)
-
-
-@ddt
-class TestInclinometer(TestCase):
-    @data(
-        (10, 100),
-        (89, 179),
-        (90, 0),
-        (180, -90),
-        (269, -179),
-        (270, 0),
-        (271, 1),
-        (360, 90),
-    )
-    @unpack
-    def test_measure_is_adapted(self, degrees, expected_degrees):
-        lsm303 = MagicMock()
-        inclinometer = Inclinometer(lsm303)
-        inclinometer.vector_2_degrees = MagicMock(return_value=(degrees))
-
-        inclination_degrees_per_axis = inclinometer.get_inclination()
-        self.assertEqual(expected_degrees, inclination_degrees_per_axis[0])
-        self.assertEqual(expected_degrees, inclination_degrees_per_axis[1])
